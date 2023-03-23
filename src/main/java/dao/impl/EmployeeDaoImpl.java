3038145167feec234997522daeccb05cf9064cd4
SIDORYCH <vladimiriv@hotmail.com>
2023-03-24 02:48:58 +0300

    Hibernate: введение, Entity . ДЗ

--- src/main/java/dao/impl/EmployeeDaoImpl.java
+++ src/main/java/dao/impl/EmployeeDaoImpl.java
@@ -1,141 +1,55 @@
 package dao.impl;
 
+import config.HibernateSessionFactoryUtil;
 import dao.EmployeeDao;
 import model.Employee;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeDaoImpl implements EmployeeDao {
-    private final Connection connection;
-
-    public EmployeeDaoImpl(Connection connection) {
-        this.connection = connection;
-    }
-
     @Override
     public void create(Employee employee) {
-
-        String sql = "INSERT INTO employee(first_name, last_name, gender, age, city_id) " +
-                "VALUES ((?), (?), (?), (?), (?))";
-
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-
-            statement.setString(1, employee.getFirstName());
-            statement.setString(2, employee.getLastName());
-            statement.setString(3, employee.getGender());
-            statement.setInt(4, employee.getAge());
-            statement.setLong(5, employee.getCityId());
-//            statement.executeQuery();
-            int x = statement.executeUpdate();
-
-            if (x > 0) {
-                System.out.println("Запись добавлена");
-            }
-            else {
-                System.out.println("Запись не добавлена");
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.save(employee);
+            transaction.commit();
         }
+
     }
 
     @Override
     public Employee readById(int id) {
 
-        Employee employee = new Employee();
-
-        String sql = "SELECT * FROM employee WHERE id = (?)";
-
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-
-            statement.setInt(1, id);
-            ResultSet resultSet = statement.executeQuery();
-
-            while (resultSet.next()) {
-                employee.setId(resultSet.getInt("id"));
-                employee.setFirstName(resultSet.getString("first_name"));
-                employee.setLastName(resultSet.getString("last_name"));
-                employee.setGender(resultSet.getString("gender"));
-                employee.setAge(resultSet.getInt("age"));
-                employee.setCityId(resultSet.getInt("city_id"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            return session.get(Employee.class, id);
         }
-        return employee;
     }
 
     @Override
     public List<Employee> readAll() {
 
-        List<Employee> employees = new ArrayList<>();
-        String sql = "SELECT * FROM employee";
-
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            ResultSet resultSet = statement.executeQuery();
-
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String firstName = resultSet.getString("first_name");
-                String lastName = resultSet.getString("last_name");
-                String gender = resultSet.getString("gender");
-                int age = resultSet.getInt("age");
-                int cityId = resultSet.getInt("city_id");
-                employees.add(new Employee(id, firstName, lastName, gender, age, cityId));
-
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return employees;
-    }
-
-    @Override
-    public void updateById(int id, Employee employee) {
-        String sql = "UPDATE employee SET first_name = (?), last_name = (?), gender = (?), age = (?), city_id = (?) WHERE id = (?)";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setString(1, employee.getFirstName());
-            statement.setString(2, employee.getLastName());
-            statement.setString(3, employee.getGender());
-            statement.setInt(4, employee.getAge());
-            statement.setInt(5, employee.getCityId());
-            statement.setInt(6, id);
-
-            int x = statement.executeUpdate();
-
-            if (x > 0) {
-                System.out.println("Запись обновлена");
-            }
-            else {
-                System.out.println("Запись не обновлена");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        try ( Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Employee", Employee.class).list();
         }
     }
 
     @Override
-    public void deleteById(int id) {
-        String sql = "DELETE FROM employee WHERE id = (?)";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+    public void update(Employee employee) {
+        try ( Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.update(employee);
+            transaction.commit();
+        }
+    }
 
-            statement.setInt(1, id);
-            int x = statement.executeUpdate();
-
-            if (x > 0) {
-                System.out.println("Запись уалена");
-            }
-            else {
-                System.out.println("Запись не удалена");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+    @Override
+    public void delete(Employee employee) {
+        try ( Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.delete(employee);
+            transaction.commit();
         }
     }
 }
--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -1,7 +1,5 @@
 package model;
 
-import org.hibernate.annotations.Table;
-
 import javax.persistence.*;
 import java.util.Objects;
 
@@ -11,6 +9,7 @@ import java.util.Objects;
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
     @Column(name = "first_name")
     private String firstName;
